1db2323c508f3740c664149c439bb13de9c093ef
PronouncedNg <bricefive@gmail.com>
2017-06-13 17:01:57 -0700

    Add files via upload
    added code in server.js so that non-existent pages route to 404 error page.

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var consolesData = require('./ConsoleData');
+var app = express();
+var port = process.env.PORT || 3000;
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'partials')));
+
+app.get('/', function(req,res) {
+	res.sendFile(path.join(__dirname+'/public/mainPage.html'));
+});
+
+app.get('*', function(req, res) {
+	res.render('404Page');
+});
+
+app.listen(port, function(){
+	console.log("== Server listening on port", port);
+});
