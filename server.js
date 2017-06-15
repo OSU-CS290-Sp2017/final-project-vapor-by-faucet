663a41bf1ab0d737758d6a95c969a3e4af7acd76
DavidSahni <sahnid@oregonstate.edu>
2017-06-15 00:42:01 -0700

    added comment support

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyparser = require('body-parser');
 
 var gameData = require('./gameData');
 
@@ -13,8 +14,35 @@ var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(bodyparser.json());
+
+
 app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname+'/public/mainPage.html'));
+});
+
+app.post('/:console/:game/newcomment', function(req, res, next){
+	var cons = req.params.console;
+	var game = req.params.game;
+	var gameinfo = gameData[cons].games[game];
+	if (gameinfo){
+		if (req.body){
+			var comment = {
+				comment: req.body.comment,
+				author: req.body.author,
+				rating: req.body.rating
+			};
+			gameinfo.comments = gameinfo.comments || [];
+			gameinfo.comments.push(comment);
+			fs.writeFile('gameData.json', JSON.stringify(gameData), function(err){
+				if (err)
+					res.status(500).send("Unable to save comment to database");
+				else
+					res.status(200).send();
+			});
+		} else
+					res.status(400).send("Request has no body");
+	} else next();
 });
 
 app.get('/:console', function (req, res, next) {
@@ -99,15 +127,22 @@ app.get('/:console/:game', function(req, res, next){
 });
 
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'partials')));
 
 
 
+
 app.get('*', function(req, res) {
 	res.render('404Page');
 });
+
+
+
+
+
 
 app.listen(port, function(){
 	console.log("== Server listening on port", port);
